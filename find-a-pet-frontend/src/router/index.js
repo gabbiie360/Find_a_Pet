0c94357d9539918d1d6d17305f0e8d98ea11081d
Eliasuarez04 <125226499+Eliasuarez04@users.noreply.github.com>
2025-06-06 17:58:14 -0600

    LoginRegisterView.vue
    Pagina agregada para poder Registrarse y Loguearse

--- find-a-pet-frontend/src/router/index.js
+++ find-a-pet-frontend/src/router/index.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import About from '@/components/homeComponents/About.vue'
 import ContactForm from '@/views/ContactView.vue'
+import LoginRegisterView from '@/views/LoginRegisterView.vue'
+
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/about', name: 'About', component: About },
   { path: '/contact', name: 'Contact', component: ContactForm },
+  { path: '/loginregister', name: 'LoginRegister', component: LoginRegisterView },
 ]
 
 const router = createRouter({
